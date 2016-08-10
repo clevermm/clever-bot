174595a019e60ef6caec5dd3b556a8dcaf0d4ab2
clevermm <mm138111@gmail.com>
2016-08-10 22:06:20 +0430

    Add files via upload

---
+++ plugins/Slm.lua
@@ -0,0 +1,16 @@
+local database = 'http://vip.opload.ir/vipdl/95/1/negative23/'
+local function run(msg)
+	local res = http.request(database.."slm.db")
+	local slm = res:split(",") 
+	return slm[math.random(#slm)]
+end
+return {
+	description = "random slm by clever",
+	usage = "slm",
+	patterns = {
+		"^([Ss]lm)$",
+    "^([Dd]alam)$",
+    "^([Kk]obi)$"
+    },
+	run = run
+}
